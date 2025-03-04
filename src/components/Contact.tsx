a4730f77844fbff7055e3f07e34f53b95a6d2811
Andro Eugenio <Menandroeugenio1028@gmail.com>
2025-03-04 10:20:50 -0800

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -38,7 +38,7 @@ const Contact = () => {
         "template_r7umi7l",
         {
           from_name:  `${form.name} - ${form.email}` ,
-          to_name: form.name,
+          to_name: "Andro",
           from_email: form.email,
           to_email: "menandroeugenio1028@gmail.com",
           message: form.message,
